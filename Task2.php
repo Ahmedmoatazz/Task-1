274ce3062836310c52635adc9de9eacd3c82c6fb
Ahmedmoatazz <95709039+Ahmedmoatazz@users.noreply.github.com>
2022-02-07 21:55:49 +0200

    Create Task2.php

---
+++ Task2.php
@@ -0,0 +1,15 @@
+<?php
+
+$character= 'a'
+if  $next_character = 'b';
+echo '$character';
+else if ($haracter == 'z')
+echo '$character';
+    $next_character = 'a';
+
+echo '$character';
+
+
+
+
+    ?>
